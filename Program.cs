0a4d1eba1a278078e541865f3023712175079b85
faiz <faizahmad.daiee@gmail.com>
2023-03-01 23:54:55 -0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -13,21 +13,21 @@ internal class Program
         // string token = await provider.LoginAsync("admin@admin.com", "admin@321");
         // Console.WriteLine("Token: " + token);
 
-        string token ="79|ai4Aelo4JBZs9WTLi5mp6TJoxdWgdEG6oAkemTO4";
-
+        string token = "79|ai4Aelo4JBZs9WTLi5mp6TJoxdWgdEG6oAkemTO4";
+        
         // add client
         Client? c = await provider.AddClientAsync("UNC-041", token);
         Console.WriteLine(c);
 
-        // delete client
-        // await provider.DeleteClientAsync(c!.Id, token);
-
         // update client
-        c!.Name ="FFaiz";
+        c!.Name = "FFaiz";
         c = await provider.UpdateClientAsync(c, token);
         Console.WriteLine(c);
 
-        
+        // delete client
+        await provider.DeleteClientAsync(c!.Id, token);
+
+
 
     }
 
